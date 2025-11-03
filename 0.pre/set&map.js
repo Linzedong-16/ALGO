7f3ba01879de84045e1ecfcb10283b9ca9c23299
linzex <linzedong555@outlook.com>
2025-11-04 00:47:33 +0800

    feat:最长回文子串

---
+++ 0.pre/set&map.js
@@ -0,0 +1,18 @@
+const numSet = new Set();
+numSet.add(1);
+numSet.add(1);
+numSet.add(2);
+numSet.add(2);
+numSet.add(5);
+numSet.add(9);
+console.log(numSet);
+
+numSet.forEach((item) => console.log(item));
+
+const dict = new Map();
+dict.set("dsad", 12);
+dict.set("dsad", 1);
+dict.set("sda", 145);
+dict.set("dssd", 12);
+console.log(dict);
+console.log(dict.get("sda"));
--- 最长回文子串/index.js
+++
@@ -1,1 +0,0 @@
-var longestPalindrome = function (s) { }
