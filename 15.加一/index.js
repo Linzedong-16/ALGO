b967170b699925b65c9dd4e0a4bdd999b499389d
linzex <linzedong555@outlook.com>
2025-12-02 01:33:43 +0800

    feat:加一

---
+++ 15.加一/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Array<number>} digits
+ * @returns {Array<number>} res
+ */
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++;
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+  return [1, ...digits];
+};
+
+console.log(plusOne([9, 9, 9]));
+console.log(plusOne([9, 9, 8]));
+console.log(plusOne([4, 7, 9]));
