ee89650e3cefe0e52740a44e97fac0d6aa923c39
RPK_16 <linzedong555@outlook.com>
2026-03-05 17:01:27 +0800

    feat:买股票最佳时机I

---
+++ 22.买股票最佳时机I/index.js
@@ -0,0 +1,38 @@
+/**
+ * 买卖股票最佳时机
+ * @param {number[]} prices
+ * @returns {number} prefit
+ */
+function maxProfit(prices) {
+  let maxProfit = 0;
+  let leftMinPoint = Infinity;
+  const leftPricesArr = [];
+
+  for (const price of prices) {
+    leftPricesArr.push(price);
+    leftMinPoint = Math.min(...leftPricesArr, leftMinPoint);
+    maxProfit = Math.max(maxProfit, price - leftMinPoint);
+  }
+  return maxProfit;
+}
+/**
+ * 买卖股票最佳时机(优化：减去剩余开支，减少逻辑冗余)
+ * @param {number[]} prices
+ * @returns {number} prefit
+ */
+function maxProfitV2(prices) {
+  let maxProfit = 0;
+  let leftMinPoint = Infinity;
+
+  for (const price of prices) {
+    leftMinPoint = Math.min(price, leftMinPoint);
+    maxProfit = Math.max(maxProfit, price - leftMinPoint);
+  }
+  return maxProfit;
+}
+
+Deno.test('巴菲特函数', () => {
+  console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+  console.log(maxProfitV2([7, 1, 5, 3, 6, 4]));
+  console.log(maxProfitV2([7, 6, 4, 3, 1]));
+});
