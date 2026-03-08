8b992f851ee00863a2dbe539c539bccf220bef92
RPK_16 <linzedong555@outlook.com>
2026-03-08 16:28:27 +0800

    feat:验证回文串

---
+++ 25.验证回文串/index.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {string} s
+ * @returns {boolean}
+ */
+function isPalindrome(s) {
+  s = s.replace(/[^a-zA-Z0-9]/g, '');
+  if (s.length < 2) {
+    return true;
+  }
+  s = s.toLowerCase();
+  let left = 0,
+    right = s.length - 1;
+  while (left <= right) {
+    if (s[left] === s[right]) {
+      left++;
+      right--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+Deno.test('判断回文', () => {
+  console.log(isPalindrome('A man, a plan, a canal: Panama'));
+});
