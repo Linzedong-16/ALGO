aebe606bff2cd0ba8d583bb7088cfdd72d073dbc
RPK_16 <linzedong555@outlook.com>
2026-03-10 16:53:12 +0800

    feat:旋转排序数组中的最小值

---
+++ 30.寻找旋转排序数组中最小值/index.js
@@ -0,0 +1,39 @@
+/**
+ * 数组原来是升序的，多次把最后一位提前旋转了
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMin = function (nums) {
+  if (nums.length === 1) {
+    return nums[0];
+  }
+  // 二分查找 + 反转规则
+  let left = 0;
+  let right = nums.length - 1;
+  // 未反转
+  if (nums[left] < nums[right]) {
+    return nums[0];
+  }
+
+  while (left < right) {
+    const mid = Math.floor(left + (right - left) / 2);
+
+    if (nums[mid] < nums[mid - 1]) {
+      return nums[mid];
+    }
+    if (nums[mid + 1] < nums[mid]) {
+      return nums[mid + 1];
+    }
+
+    if (nums[left] > nums[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+};
+
+Deno.test('旋转数组最小值', () => {
+  console.log(findMin([3, 4, 5, 1, 2]));
+  console.log(findMin([4, 5, 6, 7, 0, 1, 2]));
+});
