2cc3741460042e903a8b18bb59b32aac12a39f05
RPK_16 <linzedong555@outlook.com>
2026-03-12 09:48:54 +0800

    feat:括号生成

--- src/expand/括号生成/index.js
+++ src/expand/括号生成/index.js
@@ -1,6 +1,35 @@
 /**
- * TODO: 待开始
  * @param {number} n
  * @return {string[]}
  */
-const generateParenthesis = function (n) {};
+const generateParenthesis = function (n) {
+  // 回溯问题
+  const res = [];
+
+  /**
+   *
+   * @param {string} curr
+   * @param {number} left
+   * @param {number} right
+   */
+  function traceBack(curr, left, right) {
+    if (left === n && right === n) {
+      res.push(curr);
+    }
+    if (left < n) {
+      traceBack(curr + '(', left + 1, right);
+    }
+
+    if (right < left) {
+      traceBack(curr + ')', left, right + 1);
+    }
+  }
+  traceBack('(', 1, 0); // 必然的起点
+  return res;
+};
+
+import { assertEquals } from '@std/assert';
+Deno.test('回溯之括号生成', () => {
+  assertEquals(generateParenthesis(3), ['((()))', '(()())', '(())()', '()(())', '()()()']);
+  assertEquals(generateParenthesis(1), ['()']);
+});
