cdeae3b0618483eb5a7d019bb2240fa1581dadd1
RPK_16 <linzedong555@outlook.com>
2026-03-12 10:26:56 +0800

    feat:有效字母异位词

---
+++ src/base/40.有效字母异位词/index.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1);
+    } else {
+      map.set(s[i], 1);
+    }
+    if (map.has(t[i])) {
+      map.set(t[i], map.get(t[i]) - 1);
+    } else {
+      map.set(t[i], -1);
+    }
+  }
+
+  for (const [_, val] of map) {
+    if (val !== 0) {
+      return false;
+    }
+  }
+  return true;
+};
+import { assertEquals } from '@std/assert';
+Deno.test('有效字母异位词', () => {
+  assertEquals(isAnagram('ACANE', 'NEAAC'), true);
+});
