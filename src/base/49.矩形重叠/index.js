b54194a10171495287a696b6359ad4144d3d6855
RPK_16 <linzedong555@outlook.com>
2026-03-17 16:47:40 +0800

    feat:矩形重叠、比较退格字符串

---
+++ src/base/49.矩形重叠/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} rec1
+ * @param {number[]} rec2
+ * @return {boolean}
+ */
+const isRectangleOverlap = function (rec1, rec2) {
+  const [left1, down1, right1, top1] = rec1;
+  const [left2, down2, right2, top2] = rec2;
+  if (left1 >= right2 || right1 <= left2 || top1 < down2 || top2 <= down1) {
+    return false;
+  }
+  return true;
+};
+
+import { assertEquals } from '@std/assert';
+Deno.test('矩形重叠', () => {
+  assertEquals(isRectangleOverlap([0, 0, 2, 2], [1, 1, 3, 3]), true);
+});
---
+++ src/base/50.比较含退格字符串/index.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const backspaceCompare = function (s, t) {
+  let i = s.length - 1;
+  let j = t.length - 1;
+  let BSS = 0;
+  let BST = 0;
+
+  // 不能 && ，因为有可能一个到头了，但另一个全是回退
+  while (i >= 0 || j >= 0) {
+    // 对S处理
+    while (i >= 0) {
+      if (s[i] === '#') {
+        BSS++;
+        i--;
+      } else if (BSS > 0) {
+        BSS--;
+        i--;
+      } else {
+        // BBS === 0 && s[i] !== '# 为待比较字符
+        break;
+      }
+    }
+
+    while (j >= 0) {
+      if (t[j] === '#') {
+        BST++;
+        j--;
+      } else if (BST > 0) {
+        BST--;
+        j--;
+      } else {
+        break;
+      }
+    }
+    if (s[i] !== t[j]) {
+      return false;
+    }
+    i--;
+    j--;
+  }
+
+  return true;
+};
+
+import { assertEquals } from '@std/assert';
+Deno.test('比较含退格字符串', () => {
+  assertEquals(backspaceCompare('ab#c', 'ad#c'), true);
+});
