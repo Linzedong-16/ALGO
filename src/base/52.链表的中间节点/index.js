8bc7aac6f1f590f1c8ddaaf9f57d1e4316e9435b
RPK_16 <linzedong555@outlook.com>
2026-03-18 20:26:22 +0800

    feat:水果成篮、链表中间节点

---
+++ src/base/52.链表的中间节点/index.js
@@ -0,0 +1,28 @@
+import { arrayToList, listToArray } from '#utils/LinkList/LinkListUtils.js';
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next; // 速度2 ，快指针到尾，慢指针一定在中间
+    slow = slow.next; // 速度 1
+  }
+
+  return slow;
+};
+import { assertEquals } from '@std/assert';
+Deno.test('快慢指针解决中间节点', () => {
+  assertEquals(listToArray(middleNode(arrayToList([1, 2, 3, 4, 5]))), [3, 4, 5]);
+});
---
+++ src/base/53.水果成篮/index.js
@@ -0,0 +1,51 @@
+/**
+ * 滑动窗口
+ * @param {number[]} fruits
+ * @return {number}
+ */
+const totalFruit = function (fruits) {
+  /**
+   * 记录两个篮子中的水果的最后出现索引
+   * @type {Map<number,number>}
+   */
+  const map = new Map();
+
+  /**
+   * 指向篮子前一种水果的初始索引
+   */
+  let j = 0;
+
+  /**
+   * 篮子水果数，参数是fruits>=1的
+   */
+  let max = 1;
+
+  for (let i = 0; i < fruits.length; i++) {
+    // 添加篮子水果
+    map.set(fruits[i], i);
+
+    if (map.size > 2) {
+      // 找索引最小的那个，移动指针j
+      let minIdx = fruits.length - 1;
+      let fruit;
+      for (const [f, idx] of map) {
+        if (minIdx > idx) {
+          minIdx = idx;
+          fruit = f;
+        }
+      }
+      j = minIdx + 1;
+      map.delete(fruit);
+    }
+
+    // 篮子最多装多少
+    max = Math.max(max, i - j + 1);
+  }
+
+  return max;
+};
+
+import { assertEquals } from '@std/assert';
+Deno.test('水果成篮', () => {
+  assertEquals(totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]), 5);
+});
