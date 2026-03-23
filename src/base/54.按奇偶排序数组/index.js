680473f3cde0137d02fd5d6de3c4b8eec521ead0
RPK_16 <linzedong555@outlook.com>
2026-03-23 16:07:59 +0800

    feat:奇偶排序数组

---
+++ src/base/54.按奇偶排序数组/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortArrayByParity = function (nums) {
+  let left = 0,
+    right = nums.length;
+  while (left <= right) {
+    if (nums[left] % 2 === 0) {
+      left++;
+    } else if (nums[right] % 2 !== 0) {
+      right--;
+    } else {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+    }
+  }
+  return nums;
+};
+
+import { assertEquals } from '@std/assert';
+Deno.test('按奇偶排序数组', () => {
+  assertEquals(sortArrayByParity([3, 1, 2, 4]), [4, 2, 1, 3]);
+});
---
+++ src/base/55.按奇偶数排序数组/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortArrayByParityII = function (nums) {
+  let odd = 1;
+  for (let i = 0; i < nums.length; i += 2) {
+    if (nums[i] % 2 !== 0) {
+      while (nums[odd] % 2 !== 0) {
+        odd += 2;
+      }
+      [nums[i], nums[odd]] = [nums[odd], nums[i]];
+    }
+  }
+  return nums;
+};
+
+import { assertEquals } from '@std/assert';
+Deno.test('奇偶排序II', () => {
+  assertEquals(sortArrayByParityII([4, 2, 5, 7]), [4, 5, 2, 7]);
+});
